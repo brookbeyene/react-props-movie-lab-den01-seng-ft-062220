7b40bad1f6f0437be1398117195388074e3c3e3b
Brook Beyene <brookbeyene37@gmail.com>
2020-08-28 19:52:02 +0000

    Automatically backed up by Learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -39,7 +39,7 @@ export default class MovieCard extends Component {
       <div className="movie-card">
         {/* which component should receive which props? */}
         <CardFront poster={posterMap[this.props.poster]}/>
-        <CardBack genres={this.props.genres} title={this.props.title} IMDBRating={this.props.IMDBRating}/>
+        <CardBack  title={this.props.title} IMDBRating={this.props.IMDBRating genres={this.props.genres}}/>
       </div>
     )
   }
